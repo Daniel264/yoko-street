93f15423aa7f796ef6088057bf7ec91aaa959a93
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 16:06:55 +0100

    Refactor Menu component to improve layout and add descriptive text

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,13 @@
 export const Menu = () => {
     return (
-        <div className="flex w-full flex-row justify-center pt-10">
-            <div className="bg-[#E0F2C3] px-7 py-3 rounded-full border-2 border-black border-opacity-70">
+        <div className="flex w-full flex-col justify-center items-center pt-10">
+            <div className="rounded-full border-2 border-black border-opacity-70 bg-[#E0F2C3] px-7 py-3">
                 <h1 className="clash-font text-6xl lg:text-7xl">Explore the Menu</h1>
             </div>
             <div>
-                <p></p>
+                <p className="text-xl">
+                    Chilled Noodles, Spicy Miso Ramen with Seasonal <br /> Veggies, and Ume (Plum) Teriyaki, celebrating peak <br /> plum season.
+                </p>
             </div>
         </div>
     );
