8155fff3d79412172837c1139297cdf7c0e3b8d1
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 18:20:52 +0100

    Refactor Order and Summer components for responsive styling

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export const Order = () => {
     return (
-        <div className="flex w-full flex-row justify-center gap-5 pt-8 px-5">
-            <div className="bg-[#E0F2C3] px-7">
+        <div className="flex w-full flex-row justify-center gap-5 px-5 pt-8">
+            <div className="flex bg-[#E0F2C3] px-7 ">
                 <Image src="/assets/images/plate.svg" alt="Plate of food" width={100} height={100} />
             </div>
             <div className="border-y-2 border-black">
-                <p className="mt-5  text-xl">
+                <p className="text-sm sm:mt-5  sm:text-xl">
                     Place your order by<span className="font-semibold"> Tuesday 11:59pm EST</span> <br /> for delivery the following week (Tuesday 7/9 or Wednesday 7/10).
                 </p>
             </div>
--- src/components/Summer.tsx
+++ src/components/Summer.tsx
@@ -5,7 +5,7 @@ export const Summer = () => {
     return (
         <div className="flex w-full flex-col justify-center border-t-2 border-black pt-5">
             <div className="flex w-full flex-row justify-center">
-                <h1 className="clash-font tracking-wider sm:text-5xl md:text-6xl lg:text-7xl">Summer is for</h1>
+                <h1 className="clash-font tracking-wider text-2xl sm:text-5xl md:text-6xl lg:text-7xl">Summer is for</h1>
                 <div className="flex flex-row rounded-2xl border-2 border-black border-opacity-50 bg-[#73E6FF] pl-6 sm:rounded-full md:rounded-full ">
                     <Image src="/assets/images/white-cloud.svg" alt="White-cloud" width={70} height={200} />
                     <Image className="pb-6" src="/assets/images/sm-cloud.svg" alt="Small white cloud" width={50} height={50} />
