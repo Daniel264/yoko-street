142e8cbbee571869728c9da8fb54a6eaedfe1ad0
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 14:04:10 +0100

    Update Header component with new logo size and center alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export const Header = () => {
     return (
-        <header className="flex items-center justify-between p-5">
-            <h1 className="text-3xl font-semibold">Header</h1>
-            <Image src="/assets/logo/Logoo.svg" alt="Logo" width={50} height={50} />
+        <header className="flex w-full justify-center p-5">
+            {/* <h1 className="text-3xl font-semibold">Header</h1> */}
+            <Image src="/assets/logo/Logoo.svg" alt="Logo" width={100} height={100} />
         </header>
     );
 };
