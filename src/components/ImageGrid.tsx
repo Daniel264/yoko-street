1a7451ee0c12de1d3f9807aa4099c1b7e5a01e21
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 16:56:15 +0100

    add conditional rendering to to display either a text or image

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -2,18 +2,27 @@ import Image from "next/image";
 
 const ImageData = [
     {
+        type: "image",
         src: "/assets/images/Miso.svg",
         alt: "White-cloud",
     },
+    {
+        type: "text",
+        content: "Text"
+    }
 ];
 
 export const ImageGrid = () => {
     return (
         <div>
             <div className="grid grid-cols-2 gap-4">
-                {ImageData.map((image) => (
-                    <div key={image.src}>
-                        <Image src={image.src} alt={image.alt} width={100} height={100} />
+                {ImageData.map((item, index) => (
+                    <div key={index} className="w-full">
+                        {item.type === "image" ? (
+                            <Image src={item.src} alt={item.alt} width={500} height={100} />
+                        ) : (
+                            <p>{item.content}</p>
+                        )}
                     </div>
                 ))}
             </div>
