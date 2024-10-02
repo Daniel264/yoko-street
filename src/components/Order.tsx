95153cab41f3ac703f55777229cff99a88596ef0
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 15:46:21 +0100

    add paddings and margins to align content well

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export const Order = () => {
     return (
-        <div className="flex w-full flex-row justify-center pt-8">
+        <div className="flex w-full flex-row justify-center gap-5 pt-8 px-5">
             <div className="bg-[#E0F2C3] px-7">
                 <Image src="/assets/images/plate.svg" alt="Plate of food" width={100} height={100} />
             </div>
-            <div>
-                <p className="text-xl">
+            <div className="border-y-2 border-black">
+                <p className="mt-5  text-xl">
                     Place your order by<span className="font-semibold"> Tuesday 11:59pm EST</span> <br /> for delivery the following week (Tuesday 7/9 or Wednesday 7/10).
                 </p>
             </div>
