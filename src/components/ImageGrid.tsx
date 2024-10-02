bba4f4f0922f463758686f6cdec42260862e6fe5
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 17:07:46 +0100

    Refactor ImageGrid component to improve rendering and add default values

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -15,10 +15,10 @@ const ImageData = [
 export const ImageGrid = () => {
     return (
         <div>
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-2">
                 {ImageData.map((item, index) => (
-                    <div key={index} className="w-full">
-                        {item.type === "image" ? <Image src={item.src} alt={item.alt} width={500} height={100} /> : <p className="clash-font text-4xl font-medium tracking-wide leading-normal">{item.content}</p>}
+                    <div key={index} className="w-full border-y-2 border-black">
+                        {item.type === "image" ? <Image className="w-full" src={item.src || "/default-image-path.svg"} alt={item.alt || "default alt text"} width={500} height={100} /> : <p className="clash-font text-4xl font-semibold leading-normal tracking-wide">{item.content}</p>}
                     </div>
                 ))}
             </div>
