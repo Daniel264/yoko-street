eec05830c505a716861d4e2232347daace05e346
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 14:24:16 +0100

    Update Summer component with border and padding styles

--- src/components/Summer.tsx
+++ src/components/Summer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Summer = () => {
     return (
-        <div className="flex flex-row w-full justify-center">
+        <div className="flex flex-row w-full justify-center border-t-2 pt-5 border-black">
             <h1 className="clash-font text-7xl tracking-wider">Summer is for</h1>
             <div className="flex flex-row rounded-2xl sm:rounded-full md:rounded-full bg-[#73E6FF] pl-6 border-2 border-black border-opacity-50 ">
                 <Image src="/assets/images/white-cloud.svg" alt="White-cloud" width={70} height={200} />
