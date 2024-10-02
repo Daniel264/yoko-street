f60f5e930a43a9799d120ccb68a1358772b51f1e
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 22:42:01 +0100

    Refactor image filename in Header and Footer components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
         <footer className="mt-10 flex w-full flex-col items-center justify-center bg-[#705C35] p-5">
             {/* <h1 className="text-3xl font-semibold">Header</h1> */}
-            <Image src="/assets/logo/logoo.svg" alt="Logo" width={100} height={100} />
+            <Image src="/assets/logo/Logoo.svg" alt="Logo" width={100} height={100} />
             <div className="flex w-full justify-center gap-4 border-t-2 border-white border-opacity-50 pt-8">
                 <Image src="/assets/images/instagram.svg" alt="Instagram" width={20} height={50} />
                 <Image src="/assets/images/linkedin.svg" alt="Instagram" width={20} height={50} />
