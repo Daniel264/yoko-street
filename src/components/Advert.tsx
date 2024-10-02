c85ffdaf26d29fab833f31997bf14aab76a2d28b
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 18:28:22 +0100

    Add a span text in the Advert component and style it

--- src/components/Advert.tsx
+++ src/components/Advert.tsx
@@ -10,16 +10,16 @@ export const Advert = () => {
                 <Image src="/assets/images/right-img.svg" className="w-1/3 sm:w-full" alt="Plate of food" width={200} height={100} />
             </div>
             <div className="">
-                <h1 className="text-4xl font-medium sm:text-5xl">
-                    Last chance to order <br />
+                <h1 className="text-4xl clash-font font-medium sm:text-5xl">
+                    <span className="border-2 border-black bg-[#E0F2C3] rounded-full px-3">P.S.</span>Last chance to order <br />
                     Early Summer Bento
                 </h1>
-                <p className="mt-5  text-xl">
+                <p className="mt-5   sm:text-xl">
                     with garlic miso chicken & grilled corn rice. <br /> Get this-to-go bento for your summer picnic!
                 </p>
             </div>
             <div>
-                <Button text={"ORDER NOW"} bgColor={"pink-400"} />
+                <Button text={"ORDER NOW"} bgColor="pink-400" />
             </div>
         </div>
     );
