4b03dbcd9e1afa6f76e6ad1dd82073379187286d
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 17:30:35 +0100

    Refactor Footer component to add social media icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,13 @@ import Image from "next/image";
 
 export const Footer = () => {
     return (
-        <footer className="flex w-full justify-center mt-10 bg-[#705C35] p-5">
+        <footer className="mt-10 flex w-full flex-col items-center justify-center bg-[#705C35] p-5">
             {/* <h1 className="text-3xl font-semibold">Header</h1> */}
             <Image src="/assets/logo/Logoo.svg" alt="Logo" width={100} height={100} />
+            <div className="flex w-full justify-center gap-4 border-t-2 border-white border-opacity-50 pt-8">
+                <Image src="/assets/images/instagram.svg" alt="Instagram" width={20} height={50} />
+                <Image src="/assets/images/Linkedin.svg" alt="Instagram" width={20} height={50} />
+            </div>
         </footer>
     );
 };
