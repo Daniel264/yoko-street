e1d513e4ff9594ef8f3751c304e43bbf0940255c
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 17:18:21 +0100

    Add paddings to the menu component to improve layout

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,13 +6,13 @@ export const Menu = () => {
             <div className="rounded-full border-2 border-black border-opacity-70 bg-[#E0F2C3] px-7 py-3">
                 <h1 className="clash-font text-6xl lg:text-7xl">Explore the Menu</h1>
             </div>
-            <div>
+            <div className="pt-10">
                 <p className="text-xl">
                     Chilled Noodles, Spicy Miso Ramen with Seasonal <br /> Veggies, and Ume (Plum) Teriyaki, celebrating peak <br /> plum season.
                 </p>
             </div>
 
-            <div className="w-full px-7 lg:px-28">
+            <div className="w-full px-7 pt-10 lg:px-28">
                 <ImageGrid />
             </div>
         </div>
