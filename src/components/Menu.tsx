840dee5ee2e5c001048a436fb0563d45d821de52
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 16:04:11 +0100

    Add background color to the bowl image in Menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,9 @@
 export const Menu = () => {
     return (
         <div className="flex w-full flex-row justify-center pt-10">
-            <h1 className="clash-font text-6xl lg:text-7xl">Explore the Menu</h1>
+            <div className="bg-[#E0F2C3] px-7 py-3 rounded-full border-2 border-black border-opacity-70">
+                <h1 className="clash-font text-6xl lg:text-7xl">Explore the Menu</h1>
+            </div>
             <div>
                 <p></p>
             </div>
