b6f0bc080f0c605091a628bc0354392eeca3116e
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-02 14:03:10 +0100

    Add logo to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export const Header = () => {
     return (
         <header className="flex items-center justify-between p-5">
             <h1 className="text-3xl font-semibold">Header</h1>
+            <Image src="/assets/logo/Logoo.svg" alt="Logo" width={50} height={50} />
         </header>
     );
 };
